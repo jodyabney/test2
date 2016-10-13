4a5de5991afff55e593535bc16a2d0309db5e3fd
Jody P. Abney <jody_abney@merck.com>
2016-10-13 13:50:54 -0400

    Added hello world

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Another hello world message!")
     }
 
     override func didReceiveMemoryWarning() {
