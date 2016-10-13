fb5862a94a6bd7b11c1bf6a4eedf7b6d8af91e23
Jody P. Abney <jody_abney@merck.com>
2016-10-13 14:12:59 -0400

    Changed hellow message

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Another hello world message!")
+        print("Third hello world message!")
     }
 
     override func didReceiveMemoryWarning() {
